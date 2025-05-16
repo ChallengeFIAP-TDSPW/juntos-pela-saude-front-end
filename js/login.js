17883b2fd655962d984fe8fa1d7c6ee89f723ab0
Anna <annaclararussoluca@gmail.com>
2025-05-16 00:13:24 -0300

    Criando a funcionalidade de cadastro e login

---
+++ js/login.js
@@ -0,0 +1,85 @@
+function getUsuarios(){
+    const usuariosJSON = localStorage.getItem('usuarios');
+    return usuariosJSON ? JSON.parse(usuariosJSON) : [];
+}
+
+function salvarUsuarios(usuarios){
+    localStorage.setItem('usuarios', JSON.stringify(usuarios));
+}
+
+function mostrarMensagem (msg, cor){
+    const div = document.getElementById('mensagem');
+    div.textContent = msg;
+    div.style.color = cor;
+}
+
+// Cadastro 
+const formCadastro = document.getElementById('form_cadastro');
+if (formCadastro) {
+    formCadastro.addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        const nome = document.getElementById('nome').value;
+        const email = document.getElementById('email').value;
+        const cpf = document.getElementById('cpf').value;
+        const senha = document.getElementById('senha').value;
+        const confirmaSenha = document.getElementById('confirma_senha').value;
+
+        const usuarios = getUsuarios();
+
+        const existe = usuarios.some(user => user.email === email);
+
+        if (existe) {
+            mostrarMensagem('Email já cadastrado!', 'red');
+            return;
+        }
+
+        if (confirmaSenha === senha) {
+            usuarios.push({ nome, email, cpf, senha });
+            salvarUsuarios(usuarios);
+
+            mostrarMensagem('Cadastro realizado com sucesso!', 'green');
+            formCadastro.reset();
+
+            setTimeout(() => {
+                window.location.href = '../pages/login.html';
+            }, 1500);
+
+        } else {
+            mostrarMensagem('As senhas não coincidem. Por favor, verifique.', 'red');
+        }
+    });
+}
+
+// Login
+const formLogin = document.getElementById('form_login');
+if (formLogin) {
+    formLogin.addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        const email = document.getElementById('email').value;
+        const senha = document.getElementById('senha').value;
+
+        const usuarios = getUsuarios();
+
+        const usuario = usuarios.find(user => user.email === email);
+
+        if (!usuario) {
+            mostrarMensagem('Email não cadastrado!', 'red');
+            formLogin.reset();
+            
+        } else if (usuario.senha === senha) {
+            mostrarMensagem('Login realizado com sucesso!', 'green');
+            formLogin.reset();
+
+            setTimeout(() => {
+                window.location.href = '../index.html';
+            }, 1500);
+
+        } else {
+            mostrarMensagem('Senha incorreta!', 'red');
+        }
+
+    });
+}
+
